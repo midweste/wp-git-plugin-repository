124f17eaae926d1d1c51b321c9a412ed032f1057
midwestE <6331501+midweste@users.noreply.github.com>
2023-07-21 15:35:32 -0500

    Update wp-git-plugin-repository.php

--- wp-git-plugin-repository.php
+++ wp-git-plugin-repository.php
@@ -5,7 +5,7 @@ namespace GitPluginRepository;
 /*
  * Plugin Name:       WordPress Github/Bitbucket Plugin Updater
  * Plugin URI:        https://github.org/midweste/wp-git-plugin-repository
- * Description:       Use a Github/Bitbucket public repo api as a plugin repository for the Wordpress built in plugin updater function
+ * Description:       Test Use a Github/Bitbucket public repo api as a plugin repository for the Wordpress built in plugin updater function
  * Author:            Midweste
  * Author URI:        https://github.org/midweste/wp-git-plugin-repository
  * Update URI:        https://raw.githubusercontent.com/midweste/wp-git-plugin-repository/main/wp-git-plugin-repository.php
